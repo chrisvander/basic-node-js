7cb015585da8b54d99aeea7303ccd600e1b88fc9
Vanderloo <212703693@logon.ds.ge.com>
2018-05-29 12:27:02 -0400

    Split up pages into partials and using EJS as templater

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile('public/views/index.html', {root: __dirname }))
+app.get('/', (req, res) => res.render('pages/index', {root: __dirname }))
 app.listen(8080, () => console.log('Listening on port 8080'))
