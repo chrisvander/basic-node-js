ef5af1720d01e65d842939d18b0d96d64fe6c618
Vanderloo <212703693@logon.ds.ge.com>
2018-05-29 12:46:30 -0400

    SASS building changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const sass = require('node-sass')
 
-app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+sass.render({ file: 'sass/style.scss' });
 app.get('/', (req, res) => res.render('pages/index', {root: __dirname }))
 app.listen(8080, () => console.log('Listening on port 8080'))
